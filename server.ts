4ec3485f7dbc4686b85b9893cf60fe1297c8a47c
Михаил Гурьянов <m.guryanov@timeweb.ru>
2019-02-22 11:13:04 +0300

    Авторизация базовый механизм на сервере

--- server.ts
+++ server.ts
@@ -1,17 +1,62 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as expressJwt from 'express-jwt';
+import * as jwt from 'jsonwebtoken';
+import * as cookieParser from 'cookie-parser';
+import User from './shared/models/User';
 
-const app = express();
+const app: express.Application = express();
+const secret = 'secret';
+const root: User = {
+    id: 1,
+    username: 'root',
+    password: '111111',
+    token: '',
+};
 
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+    expressJwt({
+        secret,
+        getToken: function fromHeaderOrQuerystring(req: express.Request) {
+            let token: string | null = null;
+            if (req.cookies['X-Token']) {
+                token = req.cookies['X-Token'];
+            } else if (req.headers['x-token']) {
+                token = req.headers['x-token'] as string;
+            }
+            return token === root.token ? token : null;
+        },
+    }).unless({ path: ['/api/users/authentificate', '/api/users/logout'] })
+);
 
-app.post('/api/users/authentificate', () => {});
-app.post('/api/users/register', () => {});
-app.get('/api/users', (req: any, res: any) => {
-    // res.json({});
+app.post('/api/users/authentificate', (req: express.Request, res: express.Response) => {
+    if (req.body.username === root.username && req.body.password === root.password) {
+        root.token = jwt.sign({ sub: root.id }, secret);
+        res.set({ 'X-Token': root.token });
+        res.cookie('X-Token', root.token, { maxAge: 900000, httpOnly: true });
+        const { password, ...user } = root;
+        return res.json(user);
+    }
+    return res.sendStatus(403);
 });
-app.get('/api/users/:id', () => {});
-app.delete('/api/users/:id', () => {});
+
+app.get('/api/users/logout', (req: express.Request, res: express.Response) => {
+    root.token = '';
+    res.clearCookie('X-Token');
+    return res.sendStatus(204);
+});
+
+app.get('/api/', (req: express.Request, res: express.Response) => {
+    res.send('asdsadsaddsad');
+});
+
+app.use((err: express.Errback, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send('invalid token...');
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server started: http://localhost:3000/');
--- shared/models/User.ts
+++ shared/models/User.ts
@@ -1,8 +1,6 @@
-export class User {
+export default class User {
     id: number;
     username: string;
     password: string;
-    firstName: string;
-    lastName: string;
     token: string;
 }
